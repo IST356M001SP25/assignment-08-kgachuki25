6aa6c96b182318863b41613be59060793ec6b929
kgachuki25 <kgachuki@syr.edu>
2025-04-18 17:08:42 -0400

    finished dashboards

--- code/location_dashboard.py
+++ code/location_dashboard.py
@@ -6,3 +6,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 st.set_page_config(layout="wide")
+
+tickets_df = pd.read_csv("cache/tickets_in_top_locations.csv")
+
+# Getting locations for selection box
+locations = tickets_df["location"].unique()
+
+# Setting up streamlit
+st.title("Top Locations for Parking Tickets")
+location = st.selectbox("Select a location: ", options = locations)
+
+if location:
+    # Filter df based on location:
+    tickets_filtered = tickets_df[tickets_df["location"] == location]
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric("Total tickets issued: ", len(tickets_filtered))
+        ## set up pyplot
+        plot1, series1 = plt.subplots()
+        sns.barplot(data = tickets_filtered, x="dayofweek", y="count", estimator="sum", hue="hourofday", ax=series1).set_title("Tickets by Day of Week")
+        st.pyplot(plot1)
+
+    with col2:
+        st.metric("Total amount of Fines: ", f"${tickets_filtered['amount'].sum()}")
+        plot2, series2 = plt.subplots()
+        sns.lineplot(tickets_filtered, x = "hourofday", y = "count", estimator = "sum", ax = series2, errorbar = None).set_title("Tickets by Hour of Day")
+        st.pyplot(plot2)
+
+    st.map(tickets_filtered[['lat', 'lon']])
--- code/map_dashboard.py
+++ code/map_dashboard.py
@@ -12,3 +12,17 @@ CUSE = (43.0481, -76.1474)  # center of map
 ZOOM = 14                   # zoom level
 VMIN = 1000                 # min value for color scale
 VMAX = 5000                 # max value for color scale
+
+# Import top locations:
+locations_df = pd.read_csv("cache/top_locations_mappable.csv")
+
+# Set up initial map:
+map = folium.Map(location = CUSE, zoom_start = ZOOM)
+locations_gdf = gpd.GeoDataFrame(locations_df, geometry = gpd.points_from_xy(x = locations_df["lon"], y = locations_df["lat"]))
+locations_map = locations_gdf.explore(locations_gdf["amount"], cmap = "magma", m = map,
+                                      vmin = VMIN, vmax = VMAX, marker_type = "circle_marker",
+                                      marker_kwds= {"fillbool":True})
+
+# Setting up streamlit:
+st.title("Map of Top Locations for Parking Tickets")
+sf.st_folium(locations_map, height = 500, width = 500)
